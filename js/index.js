4f9b6bf8f856bfae100f8cad318e10c40e652869
Abdul Barik <abdulbarik1997m@gmail.com>
2025-02-27 03:09:22 +0600

    Quantity dynamic done

--- js/index.js
+++ js/index.js
@@ -17,3 +17,27 @@ for (const ringButton of ringButtons) {
     productImage.src = "../images/" + color + ".png";
   });
 }
+
+function selectWristSize(s) {
+  const sizes = ["S", "M", "L", "XL"];
+  for (const size of sizes) {
+    const button = document.getElementById("size-" + size);
+    const element = size;
+    if (s === element) {
+      button.classList.add("border-purple-600");
+    } else {
+      button.classList.remove("border-purple-600");
+    }
+  }
+}
+
+const quantityElements = document.querySelectorAll(".quantity-button");
+for (let btn of quantityElements) {
+  btn.addEventListener("click", function (event) {
+    const amount = event.target.innerText === "+" ? 1 : -1;
+    const quantityEelemnt = document.getElementById("quantity");
+    const currentQuantity = parseInt(quantityEelemnt.innerText);
+    const newQuantity = Math.max(0, currentQuantity + amount);
+    quantityEelemnt.innerText = newQuantity;
+  });
+}
