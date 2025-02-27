fa7743b6e7cb718d7c07147a563f6c14df412b21
Abdul Barik <abdulbarik1997m@gmail.com>
2025-02-28 02:11:04 +0600

    add to cart & checkout done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const ringButtons = document.querySelectorAll(".ring-button");
-
+let productImageBase = "../images/";
 for (const ringButton of ringButtons) {
   const ringBtn = ringButton;
 
@@ -14,7 +14,7 @@ for (const ringButton of ringButtons) {
     event.target.classList.remove("border-gray-300");
 
     const productImage = document.getElementById("product-image");
-    productImage.src = "../images/" + color + ".png";
+    productImage.src = productImageBase + color + ".png";
   });
 }
 
@@ -41,3 +41,81 @@ for (let btn of quantityElements) {
     quantityEelemnt.innerText = newQuantity;
   });
 }
+
+// add to cart
+let cartCount = 0;
+let cartItems = [];
+document.getElementById("add-to-cart").addEventListener("click", function () {
+  const quantity = parseInt(document.getElementById("quantity").innerText);
+
+  if (quantity > 0) {
+    document.getElementById("checkout-container").classList.remove("hidden");
+    cartCount = cartCount + quantity;
+    document.getElementById("cart-count").innerText = cartCount;
+
+    const selectedColorButton = document.querySelector(
+      "button.border-purple-600.w-6"
+    );
+    const selectedColor = selectedColorButton.id.split("-")[0];
+
+    const selectedSizeButtons = document.querySelector(
+      "button.border-purple-600:not(.w-6)"
+    );
+    const selectedSize = selectedSizeButtons.innerText.split(" ")[0];
+    const selectedPrice = selectedSizeButtons.innerText
+      .split(" ")[1]
+      .split("$")[1];
+
+    cartItems.push({
+      image: selectedColor + ".png",
+      title: " Classy Modern Smart Watch",
+      color: selectedColor,
+      size: selectedSize,
+      quantity: quantity,
+      price: quantity * parseInt(selectedPrice),
+    });
+
+    console.log(cartItems);
+  } else {
+    alert("Please select a quantity...");
+  }
+});
+
+document.getElementById("checkout-btn").addEventListener("click", function () {
+  const cartModal = document.getElementById("cart-modal");
+
+  const cartConatainer = document.getElementById("cart-items");
+
+  for (let i = 0; i < cartItems.length; i++) {
+    const item = cartItems[i];
+    console.log(item);
+    const row = document.createElement("tr");
+    row.classList.add("border-b");
+    row.innerHTML = `
+    <td class="py-2 px-4">
+      <div class="flex items-center space-x-2">
+        <img class="h-12 w-12 object-cover rounded-md" src="${productImageBase}${item.image}" alt="">
+        <span class="font-semibold">${item.title}</span>
+      </div>
+    </td>
+    <td class="py-2 px-4">${item.color}</td>
+    <td class="py-2 px-4">${item.size}</td>
+    <td class="py-2 px-4">${item.quantity}</td>
+    <td class="py-2 px-4">$${item.price}</td>
+    `;
+    cartConatainer.appendChild(row);
+  }
+
+  cartModal.classList.remove("hidden");
+});
+
+document
+  .getElementById("continue-shopping")
+  .addEventListener("click", function () {
+    document.getElementById("cart-modal").classList.add("hidden");
+  });
+
+document.getElementById("checkout").addEventListener("click", function () {
+  alert("Order Confirmed");
+  window.location.href = "/index.html";
+});
